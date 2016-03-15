2a225c2c4ef9795d6fb8ee565344952b44295982
R7232 <iver0605@gmail.com>
2016-03-15 09:58:45 +0800

    新增字串

--- app/src/main/java/com/example/student/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/example/student/lab02_interactive/MainActivity.java
@@ -15,49 +15,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private int mQuantity = 0 ; //初始值(數量)
+    private int mPrice = 5 ;    //初始值(價錢)
+
+
+
+    private void displayQuantity(){            //輸出數量
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view) ; //取數量值
+        quantityTextView.setText(String.valueOf(mQuantity));
+
+    }
     public void submitOrder(View view) {
-        int a = getqua() ;
-        dis(a);
 
+        displayTotalPrice();
+    }
+    private void displayTotalPrice(){          //輸出金額
+
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view) ; //取金額值
+        int total = mQuantity * mPrice ;
+
+        String myString = NumberFormat.getCurrencyInstance().format(total) ; //轉NT
+
+
+        String message = myString + (mQuantity == 0 ? "\nFree" : "\nThank you!") ;
+        priceTextView.setText(message);
     }
 
-    private void dis(int a) {
-        TextView sub = (TextView)findViewById(R.id.quantity_view); //數量ID
-        sub.setText(String.valueOf(a));
-
-        TextView pri = (TextView)findViewById(R.id.price); //總金額ID
-
-        a = getqua() ;
-        int b = 5 ;
-        int total = a * b ;
-
-        String nmstring = NumberFormat.getCurrencyInstance().format(total) ;
-        pri.setText(nmstring); //轉金錢NT
-
-
+    public void increment(View view){
+        ++mQuantity ;
+        displayQuantity();
+        resetTotolPrice();
+    }
+    public void decrement(View view){
+        --mQuantity ;
+        displayQuantity();
+        resetTotolPrice();
     }
 
+    private void resetTotolPrice(){
 
-    public void add(View view) {
-        int addint = getqua();
-        addint ++ ;
-        dis(addint);
-    }
-
-    public void abb(View view) {
-        TextView abb = (TextView)findViewById(R.id.abb);
-        int addint = getqua();
-
-        if (addint > 0){
-            addint -- ;
-            dis(addint);
-        }
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view) ;
+        priceTextView.setText("");
     }
 
 
-    private int getqua() {
-        TextView qua = (TextView)findViewById(R.id.quantity_view); //數量
-        String addstring = qua.getText().toString() ; //轉String
-        return Integer.parseInt(addstring); //轉INT
-    }
+
+   // private int getqua() {
+   //     TextView qua = (TextView) findViewById(R.id.quantity_view); //數量
+   //     String addstring = qua.getText().toString(); //轉String
+   //     return Integer.parseInt(addstring); //轉INT
+   // }
 }
